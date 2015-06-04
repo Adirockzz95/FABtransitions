a8243a1cb1c835c260a37bdf449664c8c0b462ae
Adirockzz95 <adityaking99@yahoo.co.in>
2015-06-04 09:25:46 +0530

    Bugs fixed

--- fabtransitions/src/main/java/adik/fabtransitions/RevealToolbar.java
+++ fabtransitions/src/main/java/adik/fabtransitions/RevealToolbar.java
@@ -1,14 +1,21 @@
-package adik.fabtransitions;
+package test.letstest;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 
 import com.nineoldandroids.animation.Animator;
 
@@ -33,47 +40,43 @@ public class RevealToolbar  {
    static SupportAnimator revealAnimator,hideAnimator;
    static ArcAnimator revealArc,hideArc;
 
-
-
-    public static void fab(Activity act,Drawable draw,int clr,int size){
+    public static void Fab(Activity act,Drawable draw,int clr,int size){
         activity = act;
         drawable = draw;
         color = clr;
         Size = size;
+
     }
 
-    public static FloatingActionButton create(){
+    public static FloatingActionButton Create(){
          fab = new FloatingActionButton.Builder(activity)
                 .withDrawable(drawable)
                 .withButtonColor(color)
                 .withMargins(0,0,16,16)
                 .withGravity(Gravity.END | Gravity.BOTTOM)
                 .create();
+                 fab.setVisibility(View.VISIBLE);
         return fab;
 
     }
 
-  public static boolean reveal(final View view){
+  public static boolean Reveal(final View Myview){
 
       FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,160);
       params.setMargins(0,0,0,0);
       params.gravity = ( Gravity.END | Gravity.BOTTOM);
-      view.setLayoutParams(params);
+      Myview.setLayoutParams(params);
+      Myview.setVisibility(View.VISIBLE);
+      fabX = (fab.getLeft() +fab.getRight()) / 2;
+      fabY = (fab.getTop() + fab.getBottom()) / 2;
+      revealX  = (Myview.getLeft() + Myview.getRight()) / 2;
+      revealY  = (Myview.getTop() + Myview.getBottom()) / 2;
+      revealRadius = Math.max(Myview.getWidth(),Myview.getHeight());
 
-      view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-          @Override
-          public void onGlobalLayout() {
-              fabX = (fab.getLeft() +fab.getRight()) / 2;
-              fabY = (fab.getTop() + fab.getBottom()) / 2;
-              revealX  = (view.getLeft() + view.getRight()) / 2;
-              revealY  = (view.getTop() + view.getBottom()) / 2;
-
-              revealRadius = Math.max(view.getWidth(), view.getHeight());
-
-                   revealAnimator = ViewAnimationUtils.createCircularReveal(view,fabX-145,fabY, 0, revealRadius);
-                   revealAnimator.setDuration(210);
+                   revealAnimator = ViewAnimationUtils.createCircularReveal(Myview,fabX-145,fabY, 0, revealRadius);
+                   revealAnimator.setDuration(200);
                    revealAnimator.setInterpolator(new AccelerateInterpolator());
-                   revealArc = ArcAnimator.createArcAnimator(fab,fabX-145.0f,fabY, +10, Side.LEFT);
+                   revealArc = ArcAnimator.createArcAnimator(fab,fabX-145.0f,fabY, +5, Side.LEFT);
                    revealArc.setDuration(120);
                    revealArc.setInterpolator(new AccelerateInterpolator());
                    revealArc.start();
@@ -85,10 +88,17 @@ public class RevealToolbar  {
 
                        @Override
                        public void onAnimationEnd(Animator animation) {
+
                            fab.hideFloatingActionButton();
-                           view.setVisibility(View.VISIBLE);
-                           revealAnimator.start();
                           fab.setVisibility(View.INVISIBLE);
+                           fab.setEnabled(false);
+                           revealAnimator.start();
+
+                           /*
+                           hand.postDelayed(hide,0);
+                          hand.postDelayed(invi,0);
+                           hand.postDelayed(rvl,0);
+                           */
 
                        }
 
@@ -105,36 +115,35 @@ public class RevealToolbar  {
 
 
 
-          }
-      });
-
       return  true;
 
   }
 
-    public static boolean hideReveal(final View Rootview) {
+    public static boolean HideReveal(final View Rootview){
 
-        hideAnimator= ViewAnimationUtils.createCircularReveal(Rootview,fabX-145,fabY,revealRadius,0);
-        hideAnimator.setDuration(210);
-        hideAnimator.setInterpolator(new AccelerateInterpolator());
-        hideArc = ArcAnimator.createArcAnimator(fab,fabX,fabY,-5, Side.RIGHT);
-        hideArc.setDuration(120);
-        hideArc.setInterpolator(new AccelerateInterpolator());
-        hideAnimator.start();
-        hideAnimator.addListener(new SupportAnimator.AnimatorListener() {
+                revealAnimator = ViewAnimationUtils.createCircularReveal(Rootview,fabX-145,fabY,revealRadius,0);
+                revealAnimator.setDuration(200);
+                revealAnimator.setInterpolator(new AccelerateInterpolator());
+                revealArc = ArcAnimator.createArcAnimator(fab,fabX,fabY,-5, Side.RIGHT);
+                revealArc.setDuration(120);
+                revealArc.setInterpolator(new AccelerateInterpolator());
+                revealAnimator.start();
+                revealAnimator.addListener(new SupportAnimator.AnimatorListener() {
                     @Override
                     public void onAnimationStart() {
 
                     }
 
                     @Override
-                    public void onAnimationEnd()  {
-                                fab.setVisibility(View.VISIBLE);
-                                fab.showFloatingActionButton();
-                                Rootview.setVisibility(View.INVISIBLE);
-                                hideArc.start();
+                    public void onAnimationEnd() {
 
-                            }
+                        Rootview.setVisibility(View.INVISIBLE);
+                        fab.setVisibility(View.VISIBLE);
+                        fab.setEnabled(true);
+                        fab.showFloatingActionButton();
+                          revealArc.start();
+
+                    }
 
                     @Override
                     public void onAnimationCancel() {
@@ -151,46 +160,8 @@ public class RevealToolbar  {
         return true;
 
     }
+   
 
-
-/*
-    public  static boolean ScrollReveal(final View Rootview){
-        revealAnimator = ViewAnimationUtils.createCircularReveal(Rootview,fabX,fabY,revealRadius,0);
-        revealAnimator.setDuration(210);
-        revealAnimator.setInterpolator(new AccelerateInterpolator());
-        revealArc = ArcAnimator.createArcAnimator(fab,fabX,fabY,-10, Side.RIGHT);
-        revealArc.setDuration(120);
-        revealArc.setInterpolator(new AccelerateInterpolator());
-        revealAnimator.start();
-
-        revealAnimator.addListener(new SupportAnimator.AnimatorListener() {
-            @Override
-            public void onAnimationStart() {
-
-            }
-
-            @Override
-            public void onAnimationEnd() {
-                fab.setVisibility(View.VISIBLE);
-                fab.showFloatingActionButton();
-                Rootview.setVisibility(View.INVISIBLE);
-                revealArc.start();
-
-            }
-
-            @Override
-            public void onAnimationCancel() {
-
-            }
-
-            @Override
-            public void onAnimationRepeat() {
-
-            }
-        });
-        return true;
-    }
-*/
 
 
 }
